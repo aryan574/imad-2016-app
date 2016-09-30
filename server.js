cd77b26529260be8656d16a385fba11cd7c5ee9d
aryan574 <sangamsingh.1994@gmail.com>
2016-09-30 21:23:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one':{
+        tilte: 'Article One : Aryan Singh',
+        heading: 'Article One',
+        date: 'Sept 5, 2016'
+    },
+    'article-two':{
+        tilte: 'Article Two : Aryan Singh',
+        heading: 'Article Two',
+        date: 'Sept 10, 2016'
+    },
+    'article-three':{
+        tilte: 'Article Three : Aryan Singh',
+        heading: 'Article Three',
+        date: 'Sept 5, 2016'
+    }
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var htmlTemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                <p>
+                    This the content of my first article. TThis the content of my first article. This the content of my first article. This the content of my first article. his the content of my first article. This the content of my first article. This the content of my first article. 
+                </p>
+                <p>
+                    This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. 
+                </p>
+                <p>
+                    This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. This the content of my first article. 
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'article[articleName]'));
 });
 
 app.get('/ui/style.css', function (req, res) {
