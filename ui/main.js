f379c7abf42831b67008e59bacca18e9d9eb63f4
aryan574 <sangamsingh.1994@gmail.com>
2016-10-29 00:10:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,7 @@ submit.onclick = function(){
     //Make a request to the server and send the name
     var request = new XMLHttpRequest();
     //Capture the list of names and render it as a list
+    request.onreadystatechange = function(){   
         if(request.readyState === XMLHttpRequest.DONE) {
            //Take some action
            if(request.status === 200){
