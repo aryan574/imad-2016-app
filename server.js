b703a342d556bbc6d81e90cffb98fc1e5a6db70f
aryan574 <sangamsingh.1994@gmail.com>
2016-11-27 23:06:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/ui/typewriter.js', function (req, res) {
 });
 
 app.get('/ui/portfolio', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'typewriter.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'portfolio.html'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
