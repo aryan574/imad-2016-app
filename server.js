01f51016c503899d770d0c0ffeedc99c7c395496
aryan574 <sangamsingh.1994@gmail.com>
2016-11-30 00:46:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/read', function(req, res){
         if(err){
             res.status(500).send(err.toString());
         }else{
-            res.send(JSON.stringify(result.rows));
+            res.send(JSON.stringify(result));
         }
     })
 })
